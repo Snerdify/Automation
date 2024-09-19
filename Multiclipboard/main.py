fdfd464a18c914649051a986c2af425dfc9d2915
Sanskruti Jaiswal <srjbeliever0129@gmail.com>
2024-09-19 17:37:39 +0530

    MultiClipboard functions added

--- Multiclipboard/main.py
+++ Multiclipboard/main.py
@@ -3,6 +3,17 @@ import clipboard
 import json
 
 
+# create a function that can save data from the clipbaord to a json file
+# make the function a write function , so that if the file does not exist the function will create it
+def save_data(filepath , data ):
+    with open(filepath, "w") as file :
+        json.dump(data , file)
+
+# create a function that can load data from a json file
+def load_data(filepath):
+    with open(filepath,"r")  as file:
+        return json.load(file)
+
 
 
 # following commands let you save , update , load data from the clipboard 
