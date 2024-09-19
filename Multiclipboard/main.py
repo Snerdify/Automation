1262b4eb3cdb6b09560eaf39243771c222978243
Sanskruti Jaiswal <srjbeliever0129@gmail.com>
2024-09-19 17:56:32 +0530

    MultiClipboard save command modification

--- Multiclipboard/main.py
+++ Multiclipboard/main.py
@@ -2,7 +2,8 @@ import sys
 import clipboard
 import json
 
-
+# this is the json file in which the data will be saved 
+DATA_JSON = "data.json"
 # create a function that can save data from the clipbaord to a json file
 # make the function a write function , so that if the file does not exist the function will create it
 def save_data(filepath , data ):
@@ -11,8 +12,11 @@ def save_data(filepath , data ):
 
 # create a function that can load data from a json file
 def load_data(filepath):
-    with open(filepath,"r")  as file:
-        return json.load(file)
+    try:
+        with open(filepath,"r")  as file:
+            return json.load(file)
+    except:
+        return {}
 
 
 
@@ -20,7 +24,15 @@ def load_data(filepath):
 if len(sys.argv )==2:
     command = sys.argv[1]
     if command =="save":
-        print("Save")
+        data = load_data(DATA_JSON)
+#Save command should save the current clipboard content to a file as a value pair to 
+# a key that is provided by the user
+        key = input("Enter the key for the clipboard data:")
+        data[key] = clipboard.paste()
+        save_data(DATA_JSON , data)
+
+
+        print("Data Saved")
     if command == "load":
         print("Load")
     else:
