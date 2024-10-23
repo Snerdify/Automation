341a14d1755cc08218be5346cd70b59d05ea1de0
Sanskruti Jaiswal <srjbeliever0129@gmail.com>
2024-10-23 08:50:29 +0530

    script#1

---
+++ Current/scripts/cloudfare.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+# instance of options
+options = Options()
+# headless browser argument 
+options.add_argument("--headless")
+
+# initialize webdriver
+driver = webdriver.Chrome(options=options)
+
+
+# get the url
+driver.get("https://www.datacamp.com/users/sign_in")
+
+# wait for 15 sec for page to load 
+time.sleep(15)
+
+driver.take_screenshot("screenshot.png")
+
+driver.close()
+
+
+
+
+
+
