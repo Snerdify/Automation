3e8a0a95a78628f50df893966ba9ab4cccdca227
Sanskruti Jaiswal <srjbeliever0129@gmail.com>
2024-10-25 16:50:34 +0530

    Product-list-dynamic-website

--- Dynamic/dynamic.py
+++ Dynamic/dynamic.py
@@ -6,12 +6,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
 # initialize the headless chrome driver - runs in the background without opening a window 
 service = Service()
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(service=service , options=options)
+
 
 url = "https://www.scrapingcourse.com/javascript-rendering"
 driver.get(url)
@@ -25,7 +27,30 @@ WebDriverWait(driver , 10).until(
 
 
 # after the js is rendered - print HTML 
-print(driver.page_source)
+html = driver.page_source
+
+soup = BeautifulSoup(html ,"html.parser")
+# print(html)
+# print(soup.title.text)
+# note - get_text() - only works on find() and doesnt work on find_all()
+product_list = []
+products  = soup.find_all('div', class_='product-item')
+
+for product in products:
+    name  = product.select_one('span', class_= 'product-name').get_text()
+    price = product.select_one('span', class_ ='product-price').get_text()
+
+    new_product = {
+        'name' : name  ,
+        'price' : price ,
+
+    }
+    product_list.append(new_product)
+
+print(product_list)
+
+
+
 
 
 
